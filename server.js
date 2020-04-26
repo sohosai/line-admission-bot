f88ad55ee76ba7a0fae96a82a918c781f2c0424d
Miharuk <miharuk0611@gmail.com>
2020-04-26 13:54:40 +0900

    「戻る」を全過程につけました。

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ async function handleEvent(event) {
   // 送信してきたユーザーを判別する ID
   const userId = event.source.userId;
 
-  if (event.message.text === "入会") {
+  if (event.message.text == "入会") {
     // 空の回答状況を記録
     answerStore[userId] = {};
 
@@ -63,106 +63,156 @@ async function handleEvent(event) {
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `${answer["name"]} さんですね。\n次は性別を教えてください。`,
+      text: `${answer["name"]} さんですね。\n次は性別を教えてください。\n変更したい場合は「戻る」と入力してください。`,
     });
   }
 
-  //性別 gender
-  if (answer["name"] != null && answer["gender"] == null) {
-    // 名前を記録
-    answer["gender"] = event.message.text;
-
-    return client.replyMessage(event.replyToken, {
-      type: "text",
-      text: `ご回答ありがとうございます。\n次は学類を教えてください。`,
-    });
-  }
-
-  if (event.message.text === "戻る" && answer["name"] != null) {
-    // 名前を記録
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] == null) {
     answer["name"] = null;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `名前を入力してください。`,
+      text: `取り消しました。\n名前を教えてください。`,
+    });
+  }
+
+  //性別 gender
+  if (answer["name"] != null && answer["gender"] == null && event.message.text != "戻る") {
+    answer["gender"] = event.message.text;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `ご回答ありがとうございます。\n次は学類を教えてください。\n変更したい場合は「戻る」と入力してください。`,
+    });
+  }
+
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null && answer["faculties"] == null) {
+    answer["gender"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\n性別を入力してください。`,
     });
   }
 
   //学類 faculties
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] == null) {
-    // 名前を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] == null && event.message.text != "戻る") {
     answer["faculties"] = event.message.text;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `${answer["faculties"]} 所属ですね。\n次は学籍番号を教えてください。`,
+      text: `${answer["faculties"]} 所属ですね。\n次は学籍番号を教えてください。\n変更したい場合は「戻る」と入力してください。`,
+    });
+  }
+
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] == null) {
+    answer["faculties"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\n学類を入力してください。`,
     });
   }
 
   //学籍番号 studentnumber
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] == null) {
-    // 名前を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] == null && event.message.text != "戻る") {
     answer["studentnumber"] = event.message.text;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `${answer["studentnumber"]} ですね。\n次はメールアドレスを教えてください。`,
+      text: `${answer["studentnumber"]} ですね。\n次はメールアドレスを教えてください。\n変更したい場合は「戻る」と入力してください。`,
+    });
+  }
+
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] == null) {
+    answer["studentnumber"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\n学籍番号を入力してください。`,
     });
   }
 
   //メアド  address
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] == null) {
-    // 名前を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] == null && event.message.text != "戻る") {
     answer["address"] = event.message.text;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `${answer["address"]} ですね。\n次は希望する局を希望する順に３つ教えてください。`,
+      text: `${answer["address"]} ですね。\n次は希望する局を希望する順に３つ教えてください。\n変更したい場合は「戻る」と入力してください。`,
     });
   }
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null  && answer["belongs"] == null) {
+    answer["address"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\nメールアドレスを入力してください。`,
+    });
+  }
+
   //希望する局（第3まで） belongs
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] == null) {
-    // 名前を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] == null && event.message.text != "戻る") {
     answer["belongs"] = event.message.text;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `希望局は順に${answer["belongs"]} ですね。\n次に特技や資格など、アピールしたいことがあれば教えてください。`,
+      text: `希望局は順に${answer["belongs"]} ですね。\n次に特技や資格など、アピールしたいことがあれば教えてください。\n変更したい場合は「戻る」と入力してください。`,
+    });
+  }
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null  && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] == null) {
+    answer["belongs"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\n希望する局を希望する順に３つ入力してください。`,
     });
   }
 
   //特技や資格など、アピールしたいこと appeal
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] == null) {
-    // 名前を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] == null && event.message.text != "戻る") {
     answer["appeal"] = event.message.text;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `了解しました。\n次はバイトや他のサークルなどを検討しているか教えてください。`,
+      text: `了解しました。\n次はバイトや他のサークルなどを検討しているか教えてください。\n変更したい場合は「戻る」と入力してください。`,
+    });
+  }
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null  && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] == null) {
+    answer["appeal"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\nアピールしたいことを入力してください。`,
     });
   }
 
   //バイトとか他のサークルとか検討してますか others
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] == null) {
-    // 名前を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] == null && event.message.text != "戻る") {
     answer["others"] = event.message.text;
 
     return client.replyMessage(event.replyToken, {
       type: "text",
-      text: `了解しました。\n最後にその他伝えたいことがあれば教えてください。`,
+      text: `了解しました。\n最後にその他伝えたいことがあれば教えてください。\n変更したい場合は「戻る」と入力してください。`,
+    });
+  }
+  if (event.message.text == "戻る" && answer["name"] != null && answer["gender"] != null  && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] == null) {
+    answer["others"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `取り消しました。\nバイトや他のサークルなど検討していることを入力してください。`,
     });
   }
 
   //その他伝えたいこと自由記述 free
-
-  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] == null) {
-    // 学類を記録
+  if (answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] == null && event.message.text != "戻る") {
     answer["free"] = event.message.text;
 
     return client.replyMessage(event.replyToken,
       [{
         type: "text",
-        text: `ありがとうございました。\n名前：${answer["name"]} さん\n性別：${answer["gender"]}\n所属：${answer["faculties"]} \n学籍番号：${answer["studentnumber"]}\nメールアドレス：${answer["address"]}\n所属希望局：${answer["belongs"]}\nアピール：${answer["appeal"]}\n他の検討：${answer["others"]}\nその他：${answer["free"]}`,
+        text: `ありがとうございました。\n名前：${answer["name"]} さん\n性別：${answer["gender"]}\n所属：${answer["faculties"]} \n学籍番号：${answer["studentnumber"]}\nメールアドレス：${answer["address"]}\n所属希望局：${answer["belongs"]}\nアピール：${answer["appeal"]}\n他の検討：${answer["others"]}\nその他：${answer["free"]}\n\n変更したい場合は「戻る」と入力してください。\n※注意：「いいえ」と入力すると今までの全データが消去されます。`,
       },
       {
         type: "template",
@@ -193,17 +243,36 @@ async function handleEvent(event) {
 
   }
 
-  if (event.message.text === "はい" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] != null) {
+  if (event.message.text == "はい" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] != null) {
     return client.replyMessage(event.replyToken, {
       type: "text",
       text: "送信完了しました！入ってくださりありがとうございます！",
     });
   }
 
-  if (event.message.text === "いいえ" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] != null) {
+  if (event.message.text == "いいえ" && answer["name"] != null && answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] != null) {
+    answer["name"] == null
+    answer["gender"] == null
+    answer["faculties"] == null
+    answer["studentnumber"] == null
+    answer["address"] == null
+    answer["belongs"] == null
+    answer["appeal"] == null
+    answer["others"] == null
+    answer["free"] == null
+    
     return client.replyMessage(event.replyToken, {
       type: "text",
       text: "登録が中止されました。",
+    });
+  }
+
+  if (event.message.text == "戻る" && answer["name"] != null&& answer["gender"] != null && answer["faculties"] != null && answer["studentnumber"] != null && answer["address"] != null && answer["belongs"] != null && answer["appeal"] != null && answer["others"] != null && answer["free"] != null) {
+    answer["free"] = null;
+
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: `その他伝えたいことを教えてください。`,
     });
   }
 
